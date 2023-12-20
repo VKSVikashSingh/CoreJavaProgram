ee4fd5640c16691df3e65fd3ab8b73770ea0afbf
Vikash Singh <v.k.singh@globallogic.com>
2023-12-20 12:23:28 +0530

    Lab001.java

---
+++ Lab001.java
@@ -0,0 +1,15 @@
+package global;
+
+public class Lab001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		System.out.println(++a + a++);
+		System.out.println(a);
+
+		//22 and 12 output
+		
+	}
+
+}
